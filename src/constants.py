c3c73adb7e9228f7e2789faf2c32a0f5419a6d01
minimac321 <minimac321@gmail.com>
2022-11-22 10:51:49 +0200

    feat: Add black formatter. Refactor code. Add src folder. Add doc strings.

--- src/constants.py
+++ src/constants.py
@@ -9,7 +9,17 @@ mp_holistic = mp.solutions.holistic
 POSE_LANDMARKS = [m.name for m in mp_pose.PoseLandmark]
 index_to_body_mapping = {i: body_part.lower() for i, body_part in enumerate(POSE_LANDMARKS)}
 
-VERTEX_ANGLE_NAMES = ["Left_armpit_angle", "Right_armpit_angle", "Left_shoulder_angle",
-                      "Right_shoulder_angle", "Left_elbow_angle", "Right_elbow_angle",
-                      "Left_hip_angle", "Right_hip_angle", "Left_groin_angle",
-                      "Right_groin_angle", "Left_knee_angle", "Right_knee_angle"]
+VERTEX_ANGLE_NAMES = [
+    "Left_armpit_angle",
+    "Right_armpit_angle",
+    "Left_shoulder_angle",
+    "Right_shoulder_angle",
+    "Left_elbow_angle",
+    "Right_elbow_angle",
+    "Left_hip_angle",
+    "Right_hip_angle",
+    "Left_groin_angle",
+    "Right_groin_angle",
+    "Left_knee_angle",
+    "Right_knee_angle",
+]
--- src/math_utils.py
+++ src/math_utils.py
@@ -3,6 +3,11 @@ import numpy as np
 
 # Check calc_angle_between_three_2d_points([1,1], [2, 2], [3, 1]) == 90 - Good :)
 def calc_angle_between_three_2d_points(a: list[float], b: list[float], c: list[float]) -> float:
+    """
+    Calculate the vertex angle between 3 points in 3D space
+
+    :return: The angle in degrees
+    """
     a = np.array(a)
     b = np.array(b)
     c = np.array(c)
